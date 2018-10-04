6d981bb914291ad4833c9a25b77b7af9a92464f3
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-04 23:44:41 +0300

    Update sensors.c

--- sensors.c
+++ sensors.c
@@ -1,1 +1,17 @@
 #include "sensors.h"
+
+void PM_Get(const char* str, char* PM2_5, char* PM10)
+{
+	char* pos1;
+	if(strstr(str,"2"))
+	{
+		pos1=strstr(str,"2");
+		if(pos1+1=="=")
+		{
+			strncpy(PM2_5,pos1+6,2);
+			strncpy(PM10,pos1+8,2);
+		}
+		PM2_5[2]='\0';
+		PM10[2]='\0';
+	}
+}
