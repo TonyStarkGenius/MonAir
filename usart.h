dce26e8004bdb756e7fdf10d06dd49ca3d90a4c2
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-07 22:27:24 +0300

    Update usart.h

--- usart.h
+++ usart.h
@@ -1,7 +1,12 @@
 #ifndef __USART_H
 #define __USART_H
 	 
-#include "main.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_usart.h"
+#include "misc.h"
+#include "sensors.h"
 #include "stdint.h"/*To enable use uint_t*/
 
 #define RX1_BUF_SIZE 32
