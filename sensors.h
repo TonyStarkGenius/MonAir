41ed23984d293cbfffe88f7b5a03c93058546e7f
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-07 20:33:40 +0300

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -9,8 +9,8 @@ char PM_10[3];
 char PM_2_5_C[6];
 char PM_10_C[6];
 
-void PM_Get(char* str , char* PM2_5 , char* PM10);
-void Convert_PM(char* PM1, char* PM2);
+void PM_Get(char* str , char* PM2_5 , char* PM10);/* str- PM-Sensor Data, PM2_5- Array to save data of PM2.5 in INT, PM10- Array to save data of PM10 in INT*/ 
+void Convert_PM(char* PM1, char* PM2);/* PM1- Array containing INT data, PM2- Array to copy data from PM1 in CHAR*/
 
 
 #endif /* __SENSORS_H */
