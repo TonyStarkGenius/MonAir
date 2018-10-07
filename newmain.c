03a940237e98ce8bcc2272aa94ec961e5ab4d211
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-07 21:46:19 +0300

    Create newmain.c

---
+++ newmain.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+My_USART1_Init(1);//Enable USART1 with interrupts
+My_USART2_Init(0);//Enable USART2 without interrupts
+while(1)
+{
+Transmit_PM_USART2();//Transmit data about PM Concentration to USART2
+
+}
+}
