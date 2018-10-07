bf83cf79a8fc93465581ba9a5cb72f48c7bca2b8
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-07 22:29:03 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,14 +2,8 @@
 #define __MAIN_H
 
 #include "stm32f4xx.h"
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_rcc.h"
-#include "stm32f4xx_usart.h"
-#include "misc.h"
-#include "stdint.h"
 #include "afunctions.h"
 #include "usart.h"
 #include "sensors.h"
-#include <string.h>
 
 #endif /* __MAIN_H */
