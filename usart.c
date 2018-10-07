84bf314ff4203fd9156ce0161390101a07cf7bc8
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-07 19:04:36 +0300

    Update usart.c

--- usart.c
+++ usart.c
@@ -3,7 +3,7 @@
 void My_USART1_Init(void)
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART1, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 	
 	__enable_irq();
 	NVIC_InitTypeDef NVIC_InitStructure;
