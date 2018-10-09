1cd6e499475fd2e70bb43a3bf8749e9d758513d8
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-09 20:06:14 +0300

    Update gsm.h

--- new/gsm.h
+++ new/gsm.h
@@ -3,15 +3,15 @@
 
 #include "main.h"
 
-void Parse_GSM_Data(char* RX_BUF);
-void USART_Connection(void);
-void PIN_Status(void);
-void Signal_Status(void);
-void Regisration_Status(void);
-void GPRS_Status(void);
-void APN_Connection(char* apn,char* uname,char* pwd);
-void GPRS_UP(void);
-void TCP_Start(char* ip,char* port);
-void TCP_Send_Data(char* data);
+void Parse_GSM_Data(char* RX_BUF);/*parser for usable data received from gsm*/
+void USART_Connection(void);/*set usart connection up*/
+void PIN_Status(void);/*receive whether PIN is required*/
+void Signal_Status(void);/*receive signal strength*/
+void Regisration_Status(void);/*registration in network status*/
+void GPRS_Status(void);/*service gprs status*/
+void APN_Connection(char* apn,char* uname,char* pwd);/*set up APN*/
+void GPRS_UP(void);/*enable gprs*/
+void TCP_Start(char* ip,char* port);/*connect to server ip- ip-adress of server*/
+void TCP_Send_Data(char* data);/*send data to server*/
 
 #endif /* __GSM_H */
