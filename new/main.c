236d9e949958ebf1385ed9fe672a7ed14cce5db9
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-09 20:17:46 +0300

    Update main.c

--- new/main.c
+++ new/main.c
@@ -3,11 +3,12 @@
 int main(void)
 {
 	My_USART1_Init(1);//Enable USART1 with interrupts
-	My_USART2_Init(1);//Enable USART2 without interrupts
+	My_USART2_Init(1);//Enable USART2 with interrupts
+	My_USART3_Init(0);//Enable USART3 without interrupts
 	SetUP_GSM_GPRS("www.ab.kyivstar.net","\0","\0");
 	while(1)
 	{
-		Send_MonAir_Data();
+		Send_MonAir_Data_Thingspeak();//send data of PM to Thingspeak
 		Delay(60000);//Delay 60 seconds
 	}
 }
