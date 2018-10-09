6ca0862e864ab5fdffd66b8716619df9da803d83
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-09 20:13:37 +0300

    Update afunctions.h

--- new/afunctions.h
+++ new/afunctions.h
@@ -4,6 +4,6 @@
 #include "main.h"
 
 void Delay(uint32_t ms);//Delay in milliseconds
-void Error_Trap(char FLAG,char myflag);
+void Error_Trap(char FLAG,char myflag);/*if catch some error send it name to usart in cycle FLAG-name of flag which needed to be enabled, myflag- flag which show name of error*/
 
 #endif /* __AFUNCTIONS_H */
