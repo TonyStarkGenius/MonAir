207c15f2641bacd4069f695656a4ec6f671b3cda
TonyStarkGenius <36626520+TonyStarkGenius@users.noreply.github.com>
2018-10-09 19:56:56 +0300

    Update mygsm.h

--- new/mygsm.h
+++ new/mygsm.h
@@ -3,7 +3,7 @@
 
 #include "main.h"
 
-void SetUP_GSM_GPRS(char* apn,char* uname,char* pwd);
-void Send_to_TCP(char* ip,char* port,char* data);
+void SetUP_GSM_GPRS(char* apn,char* uname,char* pwd);/* apn- APN Server, uname- login for server, pwd- password for server */
+void Send_to_TCP(char* ip,char* port,char* data);/* ip- ip-adress of server to connect, data- data to transmit for server */
 
 #endif /* __MYGSM_H */
